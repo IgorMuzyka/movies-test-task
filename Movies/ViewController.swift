d71768af9be5cc7a2183149e1a65a719c2830ec6
Igor Muzyka <igormuzyka42@gmail.com>
2024-06-26 13:07:00 +0300

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Igor Muzyka on 6/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
